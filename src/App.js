ef684bad1fec8efda25f92e6e5cba05a9a82a3a4
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2021-03-08 10:14:39 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+  	inputText: ''
+  }
+  updateText = (text) => {
+      this.setState(() => ({
+        inputText: text.trim()
+      }))
+    }
+  render() {
+    const { inputText } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="container">
+          <input type="text" placeholder="Say Something" onChange={(event) => this.updateText(event.target.value)}/>
+          <p className="echo">Echo: {inputText}</p>
+          <p>This should mirror the text you typed into the input field.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
